b10b015515c4a7cebbd0859acbbc5db4f0665be5
Adrian <aguerra_57@yahoo.com>
2021-08-01 15:55:09 -0500

    display backend product data to frontend with name price and image url

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import axios from "axios";
 
 Vue.config.productionTip = false
 
@@ -8,3 +9,5 @@ new Vue({
   router,
   render: h => h(App)
 }).$mount('#app')
+
+axios.defaults.baseURL = process.env.NODE_ENV === "development" ? "http://localhost:3000" : "/";
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,48 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1> {{ message }} </h1>
+    <hr>
+    <div v-for="product in products" v-bind:key="product.id">
+    <h1> Item: {{ product.name }} </h1>
+    <h2 id="p"> Price: ${{ product.price }} </h2>
+    <img v-bind:src="product.image_url">
+    <hr>
+    </div>
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
+<style scoped>
+img {
+  width: 230px;
+  height: 200px;
 }
+#home ,#p {
+  color: red;
+}
+</style>
+
+<script>
+import axios from "axios";
+export default {
+  data: function() {
+    return {
+      message: "Welcome to all of our Products!",
+      products: [],
+    };
+  },
+  created: function() {
+    this.productsIndex();
+  },
+  methods: {
+    productsIndex: function() {
+      console.log('showing all products');
+      axios.get('http://localhost:3000/products').then(response => {
+        console.log(response.data);
+        this.products = response.data;
+      })
+    }
+  },
+};
 </script>
+
