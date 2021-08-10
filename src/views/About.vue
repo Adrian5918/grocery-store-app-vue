75215ea5400b619ae46c09da3ac0f7b451eca1f7
Adrian <aguerra_57@yahoo.com>
2021-08-09 20:11:23 -0500

    before bootstrap theme installation

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,6 +1,14 @@
 <template>
   <div class="about">
     <h1>We are <stong class="F">Fresher</stong> Groceries</h1>
+    <hr>
+    <br>
+    <br>
+    <h3> Hello, Here at fresher Groceries, our aim is to provide you the best online fresh produce shopping experience with the ease of shopping from the comfort of your own home. </h3>
+    <h4> Shopping from us is very easy, all you need to do is enter you zip code and find locate grocery stores by you to shop from! you also have the option to get directions and venture to the store yourself!</h4>
+    <br>
+    <br>
+    <hr>
   </div>
 </template>
 
